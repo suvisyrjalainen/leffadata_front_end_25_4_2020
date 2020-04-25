c2cbcc26b9eab7bafeb8249a3943de4e5601cefe
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2020-04-25 21:25:05 +0300

    ensimmainen versio 25.4.2020

---
+++ js/leffat.js
@@ -0,0 +1,6 @@
+
+function hae_elokuva(){
+  var leffan_nimi = document.getElementById("leffa").value;
+  var vuosi = document.getElementById("vuosi").value;
+  document.getElementById("elokuvan_nimi").innerHTML = leffan_nimi;
+}
