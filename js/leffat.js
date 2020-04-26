95897a916b055a735186a765bcff54b7b1c554d5
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2020-04-26 11:57:46 +0300

    elokuvan lisatty

--- js/leffat.js
+++ js/leffat.js
@@ -1,6 +1,14 @@
 
-function hae_elokuva(){
+async function hae_elokuva(){
   var leffan_nimi = document.getElementById("leffa").value;
   var vuosi = document.getElementById("vuosi").value;
-  document.getElementById("elokuvan_nimi").innerHTML = leffan_nimi;
+  var posteri = document.getElementById("leffan_posteri");
+
+  var hakurimpsu = 'http://www.omdbapi.com/?t=' + leffan_nimi + '&y=' + vuosi +'&apikey=a95f3723';
+  console.log(hakurimpsu);
+  const response = await fetch(hakurimpsu);
+  const data = await response.json();
+  console.log(data);
+  document.getElementById("elokuvan_nimi").innerHTML = data.Title;
+  posteri.src = data.Poster;
 }
